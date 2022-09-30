2645ba107aa7773260975c1a8e06778708c69c34
shrinivassab <shrinivassab@gmail.com>
2022-09-30 04:48:32 -0400

    Header, SocialIcons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,42 @@
 import React from "react";
+import {SocialIcon} from "react-social-icons";
 
 type Props = {}
 
 export default function Header({} : Props) {
     return(
-        <header>
-            <div>
+        <header className="sticky top-0 p-5 flex items-start justify-between max-w-7x1 mx-auto
+        z-20 xl:items-center
+        ">
+            <div className="flex flex-row items-center">
                 {/* Social Icons */}
+                <SocialIcon
+                    url="https://twitter.com/Shrinivassab"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://www.youtube.com/shrinivassab"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/Shrinivassab"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+            </div>
+
+            <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+                <SocialIcon
+                    className="cursor-pointer"
+                    network="email"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+                    Get in Touch
+                </p>
             </div>
         </header>
     )
