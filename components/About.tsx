3c673b3fbb405d1f95e17ffb5a6f935b9fe1d40c
shrinivassab <shrinivassab@gmail.com>
2022-10-02 11:15:26 -0400

    Skills.tsx ExperienceCard.tsx WorkExperience.tsx Updated the index.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -9,11 +9,11 @@ export default function About({}: Props) {
             initial={{
                 opacity: 0,
             }}
-            transition={{
-                duration: 1.5,
-            }}
             whileInView={{
                 opacity: 1,
+            }}
+            transition={{
+                duration: 1.5,
             }}
             className="flex flex-col relative h-screen text-center md:text-left
             md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
@@ -25,8 +27,14 @@ const Home: NextPage = () => {
         </section>
 
         {/* Experience */}
+        <section id="experience" className="snap-center">
+            <WorkExperience />
+        </section>
 
         {/* Skills */}
+        <section id="skills" className="snap-start">
+            <Skills />
+        </section>
 
         {/* Projects */}
 
